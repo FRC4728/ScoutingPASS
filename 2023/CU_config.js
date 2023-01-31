f6a6ef3e38d2864d6ff90a0af6ab0ebc095a7075
FRC4728 <60230137+FRC4728@users.noreply.github.com>
2023-01-30 18:09:11 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022carv",
+      "defaultValue": "LakeSuperior2022",
       "required": "true",
       "disabled": "true"
     },
